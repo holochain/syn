0b779028f81d707272525b695776277aa375deef
Eric Harris-Braun <eric@harris-braun.com>
2020-11-21 17:43:00 -0500

    initial test

--- tests/unit-test/sym.ts
+++ tests/unit-test/sym.ts
@@ -1,17 +1,30 @@
-import { Config } from '@holochain/tryorama'
+import { Config, InstallAgentsHapps } from '@holochain/tryorama'
 import * as _ from 'lodash'
 
 const delay = ms => new Promise(r => setTimeout(r, ms));
 
-// Configure a conductor with two agents, each with own dna
-const config = Config.gen({
-  // servicelogger: Config.dna('../servicelogger.dna.gz', null),
-  host: Config.dna('../sym-app.dna.gz', null),
-});
+const config = Config.gen();
+
+const dna = path.join(__dirname, 'syn.dna.gz')
+
+const installation: InstallAgentsHapps = [
+    // agent 0
+    [
+        // happ 0
+        [dna] // contains 1 dna
+    ]
+]
 
 module.exports = (orchestrator) => {
   orchestrator.registerScenario('FIXME', async (s, t) => {
-    const { conductor } = await s.players({ conductor: config })
-    await conductor.spawn()
+    const [conductor] = await s.players([config])
+    const [[happ]] = await conductor.installAgentsHapps(installation)
+    const res = await happ.cells[0].call('syn', 'put', {
+      path: "/foo/bar",
+      content: "baz"
+    })
+    t.equal(res.length, 39) // is a hash
+    const res = await happ.cells[0].call('syn', 'get', "/foo/bar")
+    t.equal(res.length, "baz")
   }
 }
