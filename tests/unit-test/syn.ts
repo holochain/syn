5063eda81d7773d6bb17b5e172e9b810ce0ded41
Will Harris-Braun <willharrisbraun@gmail.com>
2020-12-25 13:14:34 -0500

    Update cell names for clarity

--- tests/unit-test/syn.ts
+++ tests/unit-test/syn.ts
@@ -21,35 +21,36 @@ const installation: InstallAgentsHapps = [
 module.exports = (orchestrator) => {
     orchestrator.registerScenario('syn basic zome calls', async (s, t) => {
         const [conductor] = await s.players([config])
-        const [[happ],[alice],[bob]] = await conductor.installAgentsHapps(installation)
+        const [[me_happ],[alice_happ],[bob_happ]] = await conductor.installAgentsHapps(installation)
 
-        const me_pubkey = happ.cells[0].cellId[1]
-        const alice_pubkey = alice.cells[0].cellId[1]
-        const bob_pubkey = bob.cells[0].cellId[1]
-        const me_happ = happ.cells[0]
-        const alice_happ = alice.cells[0]
-        const bob_happ = bob.cells[0]
+        const me = me_happ.cells[0]
+        const alice = alice_happ.cells[0]
+        const bob = bob_happ.cells[0]
 
-        let sessions = await me_happ.call('syn', 'get_sessions')
+        const me_pubkey = me.cellId[1]
+        const alice_pubkey = alice.cellId[1]
+        const bob_pubkey = bob.cellId[1]
+
+        let sessions = await me.call('syn', 'get_sessions')
         t.equal(sessions.length, 0)
 
         // create initial session
-        let sessionInfo = await me_happ.call('syn', 'join_session')
+        let sessionInfo = await me.call('syn', 'join_session')
         // I created the session, so I should be the scribe
         t.deepEqual(sessionInfo.scribe, me_pubkey)
         // First ever session so content should be default content
         t.deepEqual(sessionInfo.content, {title:"", body:""})
 
         // check the hash_content zome call.
-        let hash = await me_happ.call('syn', 'hash_content', sessionInfo.content)
+        let hash = await me.call('syn', 'hash_content', sessionInfo.content)
         t.deepEqual(sessionInfo.content_hash, hash)
 
         // check get_sessions utility zome call
-        sessions = await me_happ.call('syn', 'get_sessions')
+        sessions = await me.call('syn', 'get_sessions')
         t.equal(sessions.length, 1)
 
         // check that initial snapshot was created by using the get_content zome call
-        const returned_content = await me_happ.call('syn', 'get_content', sessionInfo.content_hash)
+        const returned_content = await me.call('syn', 'get_content', sessionInfo.content_hash)
         t.deepEqual(returned_content, sessionInfo.content)
 
         // add a content change
@@ -65,25 +66,25 @@ module.exports = (orchestrator) => {
                 }
             }
         }
-        const commit_header_hash = await me_happ.call('syn', 'commit', commit)
+        const commit_header_hash = await me.call('syn', 'commit', commit)
         t.equal(commit_header_hash.length, 39) // is a hash
 
         // alice joins session
-        sessionInfo = await alice_happ.call('syn', 'join_session')
+        sessionInfo = await alice.call('syn', 'join_session')
         // alice should get my session
         t.deepEqual(sessionInfo.scribe, me_pubkey)
 
         // TODO: maybe content can't be applied from deltas by zome and should only be calculated by
         // by UI, maybe not.  If not sessionInfo should also return deltas.
         t.deepEqual(sessionInfo.content, {title:"foo title", body:"bar content"})
-        hash = await me_happ.call('syn', 'hash_content', sessionInfo.content)
+        hash = await me.call('syn', 'hash_content', sessionInfo.content)
         t.deepEqual(sessionInfo.content_hash, hash)
 
 
 
         /*        // create an initial snapshot
                   const content = {title:"foo", body:"bar"};
-                  let snapshot_hash = await me_happ.call('syn', 'put_content', content)
+                  let snapshot_hash = await me.call('syn', 'put_content', content)
                   t.equal(snapshot_hash.length, 39) // is a hash
         */
 
