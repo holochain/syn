ed64fda82e52cebab064670340770b79d946d034
Eric Harris-Braun <eric@harris-braun.com>
2020-12-31 06:59:19 -0500

    Refactor User compontnet

---
+++ ui/src/User.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let pubKeyStr = ""
+  import { scribeStr } from './stores.js';
+</script>
+<style>
+  .user {
+    display: inline-block;
+    padding: 3px;
+  }
+  .scribe {
+    border: 2px solid red !important;
+  }
+  .participant {
+    border: 2px solid black;
+  }
+</style>
+<li class="user {pubKeyStr == $scribeStr ? 'scribe' : 'participant'}">
+  {pubKeyStr.slice(-4)}
+</li>
--- ui/src/Users.svelte
+++ ui/src/Users.svelte
@@ -2,30 +2,16 @@
   import { participants } from './stores.js';
   import {arrayBufferToBase64} from './Holochain.svelte';
   import { conn, scribeStr } from './stores.js';
-
+  import User from './User.svelte';
 </script>
 <style>
-  .user {
-    display: inline-block;
-    padding: 3px;
-  }
-  .scribe {
-    border: 2px solid red !important;
-  }
-  .participant {
-    border: 2px solid black;
-  }
 </style>
 <div class="users">
   Users:
   {#if $conn && $conn.me}
-  <li class="user {$conn.me == $scribeStr ? 'scribe' : 'participant'}">
-    me: {$conn.me.slice(-4)}
-  </li>
+    <User pubKeyStr={$conn.me}/>
   {/if}
   {#each Object.keys($participants) as p}
-  <li class="user {p == $scribeStr ? 'scribe' : 'participant'}">
-      {p.slice(-4)}
-    </li>
+    <User pubKeyStr={p}/>
   {/each}
 </div>
