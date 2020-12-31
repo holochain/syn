4ac1e8daaac78e6eb5e97c02a74a45ee65f1e741
Will Harris-Braun <willharrisbraun@gmail.com>
2020-12-30 20:24:42 -0500

    WIP Make title work more like Google Docs
    lol

--- ui/src/State.svelte
+++ ui/src/State.svelte
@@ -1,11 +1,12 @@
 <script>
+  import { tick } from 'svelte';
   import {callZome, session, connection, arrayBufferToBase64} from './Holochain.svelte';
   import Holochain from './Holochain.svelte';
   let content = {
     title: 'loading session...',
     body: 'loading session',
   }
-  let newTitle = '';
+  let titleBeingTyped = '';
   let commitInProgress = false;
   let pendingDeltas = [];
   let currentCommitHeaderHash;
@@ -187,16 +188,44 @@
   }
 
   let editingTitle = false;
-  function toggleEditingTitle() {
+  function saveTitle() {
     if (editingTitle) {
-      let delta = { type: 'Title', value: newTitle};
-      requestChange(delta);
-      newTitle = "";
+      let delta = { type: 'Title', value: titleBeingTyped}
+      requestChange(delta)
+      titleBeingTyped = ''
+      editingTitle = false
     } else {
-      newTitle = content.title;
+      console.log("Can't saveTitle when it wasn't being edited!")
     }
-    editingTitle = !editingTitle;
   }
+
+  let titleEl // variable to bind the
+  async function beginEditTitle() {
+    console.log("beginning edit title")
+    titleBeingTyped = content.title // fill the field with the current title
+    editingTitle = true
+    await tick() // wait for the title input element to be created
+    titleEl.focus()
+  }
+
+  function handleTitleKeypress() {
+    // console.log("Keypress in field detected!", event)
+		if (event.key == "Enter") {
+      saveTitle()
+    } else if (event.key == "Escape") {
+      // return to past
+      console.log("test");
+      // turn off editing
+      editingTitle=false
+    }
+  }
+
+  // keep track of whether the doc is untitled
+  let untitled
+  $: untitled = (content.title === '')
+
+  let titleHover // whether the title is being hovered
+
 
   function setStateFromSession(event) {
     content = event.detail.content;
@@ -212,24 +241,49 @@
     color: blue;
   }
 
+  .title {
+    /* min-width: 10em;
+    min-height: 1em; */
+    font-weight: bold;
+
+    padding: 0.4em;
+    -webkit-padding: 0.4em 0;
+    margin: 0 0 0.5em 0;
+  }
+
+  .title-hover {
+    border: 1px dashed #eee;
+  }
+
+  /* input has to be below hover so it overrides */
+  .title-input {
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  }
+
+  .untitled {
+    color: lightgray;
+  }
+
 </style>
+
 <Holochain on:setStateFromSession={setStateFromSession} on:changeReq={changeReq} on:syncReq={syncReq} on:syncResp={syncResp} on:change={change} on:updateParticipants={updateParticipants}/>
 <div>
   <div>
     Title:
-    {#if editingTitle}
-    <input bind:value={newTitle}/>
-    {:else}
-    {content.title}
-    {/if}
-
-    <button on:click={toggleEditingTitle} change>
-    {#if editingTitle}
-    save
-    {:else}
-    change
-    {/if}
-    </button>
+    <div class="title" class:title-input={editingTitle} class:title-hover={titleHover} on:mouseenter={()=>{titleHover=true}} on:mouseleave={()=>{titleHover=false}} on:click={beginEditTitle}>
+      {#if editingTitle}
+        <input bind:value={titleBeingTyped} on:keydown={handleTitleKeypress} on:blur={saveTitle} bind:this={titleEl}/>
+      {:else}
+        <span class:untitled>
+          {#if untitled}
+            Untitled Document
+          {:else}
+            {content.title}
+          {/if}
+        </span>
+      {/if}
+    </div>
   </div>
 <textarea bind:value={content.body}/>
 </div>
@@ -239,4 +293,7 @@
   <li>lastCommitedContentHash: {lastCommitedContentHashStr}
   <li>pendingDeltas: {JSON.stringify(pendingDeltas)}
   <li>participants: {participantsPretty}
+  <li>editingTitle: {editingTitle}
+  <li>titleBeingTyped: {titleBeingTyped}
+  <li>content.title: {content.title}
 </div>
