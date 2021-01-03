7819bf1c4e4023030c5bbed122007f45b161bdc7
Eric Harris-Braun <eric@harris-braun.com>
2021-01-03 09:57:31 -0500

    Included missed Debug.svelte file

---
+++ ui/src/Debug.svelte
@@ -0,0 +1,19 @@
+<script>
+  import {session, connection, scribeStr, nextIndex} from './stores.js';
+</script>
+<style>
+</style>
+<div>
+  <h4>Debug data:</h4>
+  <ul>
+    <li>
+      {#if $connection}
+        Connected to Dna: {$connection.Dna}
+      {:else}
+        No connection
+      {/if}
+    <li>session: {JSON.stringify($session)}
+    <li>nextIndex: {$nextIndex}
+    <li>scribe: {$scribeStr}
+  </ul>
+</div>
