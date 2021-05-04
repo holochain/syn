537ac8f805c8a47cbdf8f547cdaedab33385aacf
Brian Takita <brian.takita@gmail.com>
2021-05-04 06:04:05 -0400

    @syn-ui/model: recorded_changes_b: writable$ instead of recorded_changes_T

--- ui/libs/model/src/delta/recorded_changes_b.ts
+++ ui/libs/model/src/delta/recorded_changes_b.ts
@@ -1,8 +1,7 @@
 import { _b } from '@ctx-core/object'
-import { writable$, writable$_C } from '@ctx-core/store'
+import { writable$ } from '@ctx-core/store'
 import type { ApplyDelta } from './ApplyDelta'
 export const recorded_changes_b = _b('recorded_changes', ()=>
-    new recorded_changes_T(writable$<$recorded_changes_T>([]))
+    writable$<$recorded_changes_T>([])
 )
 export type $recorded_changes_T = ApplyDelta[]
-export class recorded_changes_T extends writable$_C<$recorded_changes_T> {}
