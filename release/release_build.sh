51ac89f8a05ca37189a00bf7c1a36ebf3342be53
Will Harris-Braun <willharrisbraun@gmail.com>
2021-05-08 17:37:05 -0400

    Fix release build script
    - Build Holochain DNA
    - package Electron app dependent on OS

--- release/release_build.sh
+++ release/release_build.sh
@@ -1,7 +1,18 @@
+cd ..
+make build
+cd release
+mkdir dna
+cp ../syn.dna dna
 cd ../ui/
 npm run build
 cd ../release/ui
 rm -rf build
 cp -r ../../ui/public/build/ build/
 cd ..
-npm run package-linux
+npm i
+
+if [ "$(uname)" == "Darwin" ]; then
+  npm run package-mac
+else
+  npm run package-linux
+fi
