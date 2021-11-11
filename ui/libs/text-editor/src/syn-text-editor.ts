afc50df8d22f3a51f324704a3462e1b95fb2b595
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-11-11 18:08:17 +0100

    Switch to codemirror

--- ui/libs/text-editor/src/syn-text-editor.ts
+++ ui/libs/text-editor/src/syn-text-editor.ts
@@ -1,17 +1,22 @@
-import { html, LitElement, PropertyValues } from 'lit';
+import { html, LitElement } from 'lit';
 import { property, state } from 'lit/decorators.js';
 import { ScopedElementsMixin } from '@open-wc/scoped-elements';
-import { sharedStyles, synContext, synSessionContext } from '@syn/elements';
+import { CodemirrorMarkdown } from '@scoped-elements/codemirror';
+import {
+  ProfilesStore,
+  profilesStoreContext,
+} from '@holochain-open-dev/profiles';
+import {
+  sharedStyles,
+  synContext,
+  synSessionContext,
+  getFolkColors,
+} from '@syn/elements';
 import { contextProvided } from '@lit-labs/context';
 import type { SessionStore, SynStore } from '@syn/store';
 import { StoreSubscriber } from 'lit-svelte-stores';
-import type Quill from 'quill';
-import type { Sources } from 'quill';
-import { QuillSnow } from '@scoped-elements/quill';
-import QuillCursors from 'quill-cursors';
 
-import { quillDeltasToTextEditorDelta } from './utils';
-import type { TextEditorDelta } from './text-editor-delta';
+import { TextEditorDelta, TextEditorDeltaType } from './text-editor-delta';
 
 export class SynTextEditor<CONTENT> extends ScopedElementsMixin(LitElement) {
   @property({ attribute: 'content-path' })
@@ -28,15 +33,23 @@ export class SynTextEditor<CONTENT> extends ScopedElementsMixin(LitElement) {
   @state()
   sessionStore!: SessionStore<CONTENT, any>;
 
+  @contextProvided({ context: profilesStoreContext, multiple: true })
+  @state()
+  profilesStore!: ProfilesStore;
+
   _content = new StoreSubscriber(this, () => this.sessionStore?.content);
   _ephemeral = new StoreSubscriber(this, () => this.sessionStore?.ephemeral);
+  _allProfiles = new StoreSubscriber(
+    this,
+    () => this.profilesStore?.knownProfiles
+  );
 
   _deltasNotEmmitted: TextEditorDelta[] = [];
   _cursorPosition: number | undefined = 0;
 
   firstUpdated() {
     setInterval(() => this.emitDeltas(), this.debounceMs);
-
+    /* 
     setTimeout(() => {
       this.quill.on('selection-change', (range, _oldRange, source) => {
         console.log('hey', source, _oldRange, range);
@@ -54,17 +67,7 @@ export class SynTextEditor<CONTENT> extends ScopedElementsMixin(LitElement) {
           })
         );
       });
-    });
-  }
-
-  onTextChanged(deltas: any, source: Sources) {
-    if (source !== 'user') return;
-
-    const ops = deltas.ops;
-    if (!ops || ops.length === 0) return;
-
-    const delta = quillDeltasToTextEditorDelta(ops);
-    this._deltasNotEmmitted.push(delta);
+    }); */
   }
 
   emitDeltas() {
@@ -75,7 +78,7 @@ export class SynTextEditor<CONTENT> extends ScopedElementsMixin(LitElement) {
             deltas: this._deltasNotEmmitted,
             ephemeral: {
               [this.synStore.myPubKey]: this._cursorPosition,
-            }
+            },
           },
           bubbles: true,
           composed: true,
@@ -85,18 +88,23 @@ export class SynTextEditor<CONTENT> extends ScopedElementsMixin(LitElement) {
     }
   }
 
+  /* 
+  onTextChanged(deltas: any, source: Sources) {
+    if (source !== 'user') return;
+
+    const ops = deltas.ops;
+    if (!ops || ops.length === 0) return;
+
+    const delta = quillDeltasToTextEditorDelta(ops);
+    this._deltasNotEmmitted.push(delta);
+  }
+
+
   get quill(): Quill {
     return (this.shadowRoot?.getElementById('editor') as QuillSnow)?.quill;
-  }
+  } */
 
-  updated(changedValues: PropertyValues) {
-    super.updated(changedValues);
-
-    if (this.quill) {
-      this.updateQuill();
-    }
-  }
-
+  /* 
   updateQuill() {
     if (this.getContent()) {
       this.quill.setText(this.getContent());
@@ -120,7 +128,7 @@ export class SynTextEditor<CONTENT> extends ScopedElementsMixin(LitElement) {
         cursors.moveCursor(agentPubKey, range);
       }
     }
-  }
+  } */
 
   getContent(): string {
     let content = this._content.value;
@@ -135,26 +143,59 @@ export class SynTextEditor<CONTENT> extends ScopedElementsMixin(LitElement) {
     return content as any;
   }
 
-  render() {
-    return html`<quill-snow
-      style="flex: 1;"
-      id="editor"
-      .modules=${{ 'modules/cursors': QuillCursors }}
-      .options=${{
-        placeholder: 'Write your note...',
-        modules: {
-          cursors: true,
-        },
-      }}
-      @text-change=${e => this.onTextChanged(e.detail.delta, e.detail.source)}
-    ></quill-snow>`;
+  onTextInserted(from: number, text: string) {
+    this._deltasNotEmmitted.push({
+      type: TextEditorDeltaType.Insert,
+      text: text,
+      position: from,
+    });
   }
 
-  static get scopedElements() {
-    return {
-      'quill-snow': QuillSnow,
-    };
+  onTextDeleted(from: number, to: number) {
+    this._deltasNotEmmitted.push({
+      type: TextEditorDeltaType.Delete,
+      position: from,
+      characterCount: to - from,
+    });
+  }
+
+  onSelectionChanged(ranges: Array<{ from: number; to: number }>) {
+    this._cursorPosition = ranges[0].to;
+  }
+
+  remoteCursors() {
+    if (!this._ephemeral.value) return [];
+    return Object.entries(this._ephemeral.value as any)
+      .filter(([pubKey, _]) => pubKey !== this.synStore.myPubKey)
+      .map(([agentPubKey, position]) => {
+        const { r, g, b } = getFolkColors(agentPubKey);
+
+        const name = this._allProfiles.value[agentPubKey].nickname;
+        return {
+          position,
+          color: `${r} ${g} ${b}`,
+          name: name || 'Loading...',
+        };
+      });
+  }
+
+  render() {
+    return html`<codemirror-markdown
+      style="flex: 1;"
+      id="editor"
+      .text=${this._content.value}
+      .additionalCursors=${this.remoteCursors()}
+      @text-inserted=${e => this.onTextInserted(e.detail.from, e.detail.text)}
+      @text-deleted=${e => this.onTextDeleted(e.detail.from, e.detail.to)}
+      @selection-changed=${e => this.onSelectionChanged(e.detail.ranges)}
+    ></codemirror-markdown>`;
   }
 
   static styles = [sharedStyles];
+
+  static get scopedElements() {
+    return {
+      'codemirror-markdown': CodemirrorMarkdown,
+    };
+  }
 }
