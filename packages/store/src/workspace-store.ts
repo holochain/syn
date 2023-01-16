f5e82d52ab570bbe340a278ea1e7a08c26a74498
Eric Harris-Braun <eric@harris-braun.com>
2023-01-16 11:35:50 -0500

    bump to 0.11.12

--- packages/store/src/workspace-store.ts
+++ packages/store/src/workspace-store.ts
@@ -133,6 +133,7 @@ export class WorkspaceStore<G extends SynGrammar<any, any>>
   ) {
     this.unsubscribe = this.rootStore.client.client.on('signal',
       signal => {
+        console.log("SIGNAL", signal)
         const synSignal: SynSignal = signal.payload;
 
         if (synSignal.message.type !== 'WorkspaceMessage') return;
