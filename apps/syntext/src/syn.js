f1e50313e3f2f82aabaf8aab3ded85a4b23c3b87
Eric Harris-Braun <eric@harris-braun.com>
2023-01-26 09:49:21 -0500

    fix in syntext

--- apps/syntext/src/syn.js
+++ apps/syntext/src/syn.js
@@ -32,9 +32,7 @@ export function textSlice(workspaceStore) {
 export async function createClient() {
   //const url = `ws://localhost:${process.env.HC_PORT}`;
 
-  const appWebsocket = await AppWebsocket.connect(undefined);
-
-  const client = await AppAgentWebsocket.connect(appWebsocket, 'syn');
+  const client = await AppAgentWebsocket.connect(undefined, 'syn');
   
   return client
 }
