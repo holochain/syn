c0a802c6c0e2f63f33ea7421ccfac50d058023bb
Eric Harris-Braun <eric@harris-braun.com>
2025-07-16 10:51:26 -0400

    chore: update to hc-0.5.4

--- zomes/syn/coordinator/src/utils.rs
+++ zomes/syn/coordinator/src/utils.rs
@@ -50,3 +50,51 @@ pub fn delete_link_relaxed(address: ActionHash) -> ExternResult<ActionHash> {
             .delete_link(DeleteLinkInput::new(address, ChainTopOrdering::Relaxed))
     })
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ZomeFnInput<T> {
+    pub input: T,
+    pub local: Option<bool>,
+}
+
+impl<T> ZomeFnInput<T> {
+    pub fn new(input: T, local: Option<bool>) -> Self {
+        Self { input, local }
+    }
+
+    pub fn get_strategy(&self) -> GetStrategy {
+        let local = self.local.unwrap_or(false);
+        match local {
+            true => GetStrategy::Local,
+            false => GetStrategy::Network,
+        }
+    }
+
+    pub fn get_options(&self) -> GetOptions {
+        let local = self.local.unwrap_or(false);
+        match local {
+            true => GetOptions::local(),
+            false => GetOptions::network(),
+        }
+    }
+}
+
+impl<T> Into<GetStrategy> for ZomeFnInput<T> {
+    fn into(self) -> GetStrategy {
+        let local = self.local.unwrap_or(false);
+        match local {
+            true => GetStrategy::Local,
+            false => GetStrategy::Network,
+        }
+    }
+}
+
+impl<T> Into<GetOptions> for ZomeFnInput<T> {
+    fn into(self) -> GetOptions {
+        let local = self.local.unwrap_or(false);
+        match local {
+            true => GetOptions::local(),
+            false => GetOptions::network(),
+        }
+    }
+}
